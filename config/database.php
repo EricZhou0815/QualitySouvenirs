dd2b4ba280d54371fe7f5692d8dc24767b76d49e
Eric Zhou <eric.zhou0815@gmail.com>
2018-10-17 22:46:45 +1300

    update

--- config/database.php
+++ config/database.php
@@ -41,10 +41,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'QualitySouvenirs'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
@@ -69,8 +69,8 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '32769'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,88 +9,55 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="{{ asset('css/asite.css') }}" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <!--Navbar-->
+    <nav class="navbar navbar-expand-md navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="myLogo navbar-brand">
+                        <a asp-area="" asp-controller="Home" asp-action="Index">
+                            <div class="logoContainer">
+                                <img class="myLogoImage" src="~/images/Logo.jpg" alt="Pono" />
+                                <div class="logoText">Quality Souvenirs</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="navbar-collapse collapse">
+                        <ul class="navbar-nav ml-auto nav">
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/shop') }}">SHOP</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/about') }}">ABOUT</a></li>
+                            <li class="nav-item"><a class="nav-link" href="{{ url('/contact') }}">CONTACT</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="{{ url('/souvenir') }}">Souvenir</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="{{ url('/supplier') }}">Supplier</a></li>
+                                        <li class="nav-item"><a class="nav-link" href="{{ url('/category') }}">Category</a></li>
+                        </ul>
+                                      
+                    </div>
                 </div>
             </div>
-        </div>
+        </nav>
+
+
+
+
+
+
+
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
